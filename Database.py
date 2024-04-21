a5e2ae8105603734177045fe04ecc0cd005fa34f
Gazzaev Yaroslav Eduardovich <iegazzaev@edu.hse.ru>
2024-04-21 20:24:34 +0300

    add db and fixed few moments

--- Database.py
+++ Database.py
@@ -5,20 +5,6 @@ conn = sqlite3.connect('mydata.db')
 
 cursor = conn.cursor()
 
-# #Создание
-# cursor.execute('''CREATE TABLE users
-#                 (id INTEGER PRIMARY KEY, TG_ID INTEGER, name TEXT, referrals TEXT)''')
-#
-# #Добавление
-# cursor.execute("INSERT INTO users (TG_ID, name, referrals) VALUES (?, ?, ?)", (1040305807, 'Yaroslav', '2313203213, 2313213555, 212122123112, 55425'))
-# cursor.execute("INSERT INTO users (TG_ID, name, referrals) VALUES (?, ?, ?)", (2424243, 'yarik', ''))
-
-##Обновление данных
-# cursor.execute("UPDATE users SET age = ? WHERE name = ?", (1488, "Alice"))
-
-# cursor.execute("SELECT referrals FROM users WHERE TG_ID = ?", ('1040305807',))
-# row = cursor.fetchall()
-
 def get_referrals_count(TG_ID):
     conn = sqlite3.connect('mydata.db')
 
@@ -33,3 +19,121 @@ def get_referrals_count(TG_ID):
     except Exception as ex:
         return 0
 
+def has_referrer(TG_ID):
+    conn = sqlite3.connect('mydata.db')
+
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT id FROM users WHERE TG_ID = ?", (TG_ID,))
+    row = cursor.fetchall()
+
+    conn.commit()
+
+    conn.close()
+    print(row)
+    if row:
+        return False
+    return True
+
+
+def get_all_users():
+    conn = sqlite3.connect('mydata.db')
+
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT TG_ID FROM users")
+    tg_ids = [row[0] for row in cursor.fetchall()]
+
+    conn.commit()
+
+    conn.close()
+    return tg_ids
+
+def add_inv_ref(TGID_invaited,TGID_refowner):
+    conn = sqlite3.connect('mydata.db')
+
+    cursor = conn.cursor()
+
+    cursor.execute("UPDATE users SET inv_referral = ? WHERE TG_ID = ?", (TGID_refowner,TGID_invaited))
+
+    cursor.execute("SELECT referrals FROM users WHERE TG_ID = ?", (TGID_refowner,))
+    row = cursor.fetchall()
+
+    try:
+        new_refs = " ".join(row[0]).split(", ")
+        new_refs.append(TGID_invaited)
+        new_refs = ", ".join(new_refs)
+    except Exception:
+        new_refs = f'{TGID_invaited}, '
+
+    cursor.execute("UPDATE users SET referrals = ? WHERE TG_ID = ?", (new_refs, TGID_refowner))
+
+    conn.commit()
+
+    conn.close()
+
+
+def add_user(TG_ID, name, inv_referral):
+    conn = sqlite3.connect('mydata.db')
+
+    cursor = conn.cursor()
+
+    cursor.execute("INSERT INTO users (TG_ID, name, referrals, inv_referral) VALUES (?, ?, ?, ?)", (TG_ID, name, '', inv_referral))
+
+    conn.commit()
+
+    conn.close()
+
+
+def check_user_in_db(TG_ID):
+    conn = sqlite3.connect('mydata.db')
+
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT id FROM users WHERE TG_ID = ?", (TG_ID,))
+    row = cursor.fetchall()
+
+    conn.commit()
+
+    conn.close()
+    if row:
+        return True
+    return False
+
+# add_inv_ref('2424243','1040305807')
+
+# print(has_referrer(2543535435))
+
+# #Создание
+# cursor.execute('''CREATE TABLE users
+#                 (id INTEGER PRIMARY KEY, TG_ID TEXT, name TEXT, referrals TEXT, inv_referral TEXT)''')
+
+# #Добавление
+# cursor.execute("INSERT INTO users (TG_ID, name, referrals,inv_referral) VALUES (?, ?, ?, ?)", (1223, 'Yaroslav', '2313203213, 2313213555, 212122123112, 55425', ""))
+# cursor.execute("INSERT INTO users (TG_ID, name, referrals, inv_referral) VALUES (?, ?, ?, ?)", (2424243, 'yarik', '', ""))
+
+##Обновление данных
+# cursor.execute("UPDATE users SET age = ? WHERE name = ?", (1488, "Alice"))
+
+# cursor.execute("UPDATE users SET inv_referral = ? WHERE name = ?", ('', "yarik"))
+
+# cursor.execute("SELECT referrals FROM users WHERE TG_ID = ?", ('1040305807',))
+# row = cursor.fetchall()
+# try:
+#     new_refs = " ".join(row[0]).split(", ")
+#     new_refs.append('2424243')
+#     new_refs = ", ".join(new_refs)
+#     print(new_refs)
+# except Exception as ex:
+#     new_refs = '2424243, '
+
+# cursor.execute("SELECT TG_ID FROM users")
+# tg_ids = [row[0] for row in cursor.fetchall()]
+#
+# print(tg_ids)
+
+# cursor.execute("SELECT referrals FROM users WHERE TG_ID = ?", ('1040305807',))
+# row = cursor.fetchall()
+conn.commit()
+
+conn.close()
--- main.py
+++ main.py
@@ -28,23 +28,9 @@ TOKEN = getenv("BOT_TOKEN")
 dp = Dispatcher()
 router = Router(name=__name__)
 
-# @dp.message(CommandStart())
-# async def command_start_handler(message: Message) -> None:
-#     user_id = message.from_user.id
-#     referrer = None
-#
-#     # Проверяем наличие хоть какой-то дополнительной информации из ссылки
-#     if " " in message.text:
-#         referrer_candidate = message.text.split()[1]
-#         print(referrer_candidate)
-#     await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!. You invited by {referrer_candidate}")
-
-subscribed = False
-
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message):
-    global subscribed
     kb_start = InlineKeyboardBuilder()
     kb_start.add(InlineKeyboardButton(
         text='Подписаться',
@@ -52,24 +38,44 @@ async def command_start_handler(message: Message):
     )
     kb_start.add(InlineKeyboardButton(
         text='Проверить',
-        callback_data='link')
+        callback_data='check')
     )
 
-    await message.answer(f"Привет, {html.bold(message.from_user.full_name)}!\nДля начала выполни условия",
-                         reply_markup=kb_start.as_markup())
+    if not Database.check_user_in_db(str(message.from_user.id)):
+        Database.add_user(str(message.from_user.id), message.from_user.full_name, '')
+        if not Database.has_referrer(message.from_user.id):
+            referrer = None
+
+            if " " in message.text:
+                referrer_candidate = message.text.split()[1]
+                # print(referrer_candidate)
+                try:
+                    referrer_candidate = int(referrer_candidate)
+                    # print(message.from_user.id != referrer_candidate)
+                    # print('--------------')
+                    # print(Database.get_all_users())
+
+                    if message.from_user.id != referrer_candidate and str(referrer_candidate) in Database.get_all_users():
+                        referer = referrer_candidate
+                        Database.add_inv_ref(message.from_user.id, referer)
+                        # Database.add_user(message.from_user.id, message.from_user.full_name, referer)
+
+                except ValueError:
+                    pass
+        await message.answer(f"Привет, {html.bold(message.from_user.full_name)}!\nДля начала выполни условия",
+                             reply_markup=kb_start.as_markup())
 
 
-@dp.callback_query(F.data == "link")
-async def send_random_value(callback: types.CallbackQuery, bot: Bot):
-    global subscribed
+@dp.callback_query(F.data == "check")
+async def check_subscribe(callback: types.CallbackQuery, bot: Bot):
 
     try:
         member_status = await bot.get_chat_member(chat_id='@laslaldlsala', user_id=callback.message.chat.id)
         if member_status.status != 'left':
             await callback.answer("Вы являетесь участником канала ☑️")
-            subscribed = True
 
             ## тут дописать кем он инвайтнут
+
         else:
             await callback.answer("Вы не являетесь участником канала ❌")
     except Exception as e:
@@ -80,30 +86,23 @@ async def send_random_value(callback: types.CallbackQuery, bot: Bot):
 
 @dp.message(Command("referral"))
 async def cmd_refferal(message: types.Message):
-    global subscribed
-    if not subscribed:
-        await message.answer("Пожалуйста, подпишитесь на канал, чтобы продолжить использование бота.")
-    else:
-        ref_count = Database.get_referrals_count(message.chat.id)
-        await message.answer(
-            f"Кол-во рефов - {ref_count}.\nВаша реф ссылка - https://t.me/cerebrrrrrrr_test_bot?start={message.chat.id}")
+    ref_count = Database.get_referrals_count(message.chat.id)
+    await message.answer(
+        f"Кол-во рефов - {ref_count}.\nВаша реф ссылка - https://t.me/cerebrrrrrrr_test_bot?start={message.chat.id}")
 
 
 @dp.message(Command("random"))
 async def cmd_random(message: types.Message):
-    global subscribed
-    if not subscribed:
-        await message.answer("Пожалуйста, подпишитесь на канал, чтобы продолжить использование бота.")
-    else:
-        builder = InlineKeyboardBuilder()
-        builder.add(types.InlineKeyboardButton(
-            text="Нажми меня",
-            callback_data="random_value")
-        )
-        await message.answer(
-            "Нажмите на кнопку, чтобы бот отправил число от 1 до 10",
-            reply_markup=builder.as_markup()
-        )
+
+    builder = InlineKeyboardBuilder()
+    builder.add(types.InlineKeyboardButton(
+        text="Нажми меня",
+        callback_data="random_value")
+    )
+    await message.answer(
+        "Нажмите на кнопку, чтобы бот отправил число от 1 до 10",
+        reply_markup=builder.as_markup()
+    )
 
 
 @dp.callback_query(F.data == "random_value")
